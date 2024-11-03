350643b8a762a319ef90a0cf857b5b17a6c477d4
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-11-03 12:57:08 +0500

    добавлена анимация скрытия баннера, z-index для header карточек поставлен на изначальный (фикс влияния стилей header-навигации сайта)

--- scripts/main.js
+++ scripts/main.js
@@ -10,25 +10,30 @@ document.querySelector(`footer #up`).addEventListener(`click`, () => {
 // —————————— INDEX.PHP ——————————
 
 // реализовать поиск по викторинам
-// добавить анимацию скрытия баннера
 localStorage.setItem(`banner_hidden`, `no`);
 
-if (window.location.pathname.includes('index.php')) {
+if (window.location.pathname.includes(`index.php`)) {
 	if (localStorage.getItem(`banner_hidden`) === `yes`) {
-		// скрыть баннер если уже нажималась кнопка "понятно, спасибо!"
+		// скрыть баннер если уже нажималась кнопка `понятно, спасибо!`
 		document.querySelector(`#intro`).remove();
 	} else {
-		// кнопка "пройти" —> скролл к контейнеру викторин
+		// скролл к контейнеру викторин на кнопку `пройти`
 		document.querySelector(`#take-quiz`).addEventListener(`click`, () => {
-			document.querySelector(`#quizes-con`).scrollIntoView({
+			window.scrollTo({
+				top: document.querySelector(`#intro`).offsetHeight,
 				behavior: `smooth`,
-				block: 'start',
 			});
 		});
 
-		// кнопка "понятно, спасибо!" —> спрятать баннер-интро
+		// спрятать баннер-интро на кнопку `понятно, спасибо!`
 		document.querySelector(`#hide-banner`).addEventListener(`click`, () => {
-			document.querySelector(`#intro`).remove();
+			window.scrollTo({
+				top: document.querySelector(`#intro`).offsetHeight,
+				behavior: `smooth`,
+			});
+			setTimeout(() => {
+				document.querySelector(`#intro`).remove();
+			}, 1000);
 			localStorage.setItem(`banner_hidden`, `yes`);
 		});
 	}
@@ -49,30 +54,31 @@ if (window.location.pathname.includes('index.php')) {
 // —————————— QUIZ.PHP ——————————
 
 // добавить сохранение в localstorage
-// добавить функционал кнопке "подробнее"
+// добавить функционал кнопке `подробнее`
 
-if (window.location.pathname.includes('quiz.php')) {
-	document.addEventListener('DOMContentLoaded', () => {
-		const totalQuestions = document.querySelectorAll('.question-con').length;
-		const completedElement = document.getElementById('completed');
-		const inputs = document.querySelectorAll('.answer');
+if (window.location.pathname.includes(`quiz.php`)) {
+	document.addEventListener(`DOMContentLoaded`, () => {
+		let totalQuestions = document.querySelectorAll(`.question-con`).length;
+		let completedElement = document.querySelector(`#completed`);
+		let inputs = document.querySelectorAll(`.answer`);
 
 		function updateProgress() {
 			let answeredQuestions = Array.from(inputs).filter(
 				(input) =>
-					(input.type === 'radio' && input.checked) ||
-					(input.tagName === 'TEXTAREA' && input.value.trim() !== '')
+					(input.type === `radio` && input.checked) ||
+					(input.tagName === `TEXTAREA` && input.value.trim() !== ``)
 			).length;
 
-			const progress = Math.floor((answeredQuestions / totalQuestions) * 100);
-			completedElement.textContent = Math.max(0, Math.min(progress, 100));
+			let progress = Math.floor((answeredQuestions / totalQuestions) * 100);
+			completedElement.textContent =
+				Math.max(0, Math.min(progress, 100)) || `0`; // чтобы NaN не возвращал
 		}
 
 		inputs.forEach((input) => {
-			if (input.type === 'radio') {
-				input.addEventListener('change', updateProgress);
-			} else if (input.tagName === 'TEXTAREA') {
-				input.addEventListener('input', updateProgress);
+			if (input.type === `radio`) {
+				input.addEventListener(`change`, updateProgress);
+			} else if (input.tagName === `TEXTAREA`) {
+				input.addEventListener(`input`, updateProgress);
 			}
 		});
 

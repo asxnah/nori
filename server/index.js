6b0026503589aaebe6d5f6a92001f913c9eb3771
아수나 <105264974+asxnah@users.noreply.github.com>
2025-04-17 20:19:45 +0500

    -

--- server/index.js
+++ server/index.js
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
 	res.send('server is running');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
 	console.log(`Server is running on port ${port}`);
 });

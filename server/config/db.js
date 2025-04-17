dfe5b0eb794b7cdc579d46b3faf88552986f9ecd
아수나 <105264974+asxnah@users.noreply.github.com>
2025-04-17 20:08:07 +0500

    feat: настраиваю окружение для хостинга

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,20 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import process from 'process';
+
+dotenv.config();
 
 const connectDB = async () => {
 	try {
 		const mongoURI =
 			process.env.MONGODB_URI || 'mongodb://localhost:27017/nori';
-		await mongoose.connect(mongoURI);
-		console.debug('база данных подключена');
+		await mongoose.connect(mongoURI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.debug('База данных подключена');
 	} catch (err) {
-		console.error('ошибка в подключении базы данных: ' + err);
+		console.error('Ошибка при подключении к базе данных: ' + err);
 		process.exit(1);
 	}
 };
--- server/index.js
+++ server/index.js
@@ -3,19 +3,17 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
 import quizRoutes from './routes/quizzes.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import process from 'process';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Serve static files
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 

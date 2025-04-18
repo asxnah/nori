ae97cd71bf758ace27396d650ec7d71a31edae5b
아수나 <105264974+asxnah@users.noreply.github.com>
2025-04-18 12:53:32 +0500

    -

--- server/index.js
+++ server/index.js
@@ -5,11 +5,16 @@ import authRoutes from './routes/auth.js';
 import quizRoutes from './routes/quizzes.js';
 import dotenv from 'dotenv';
 import process from 'process';
+import path from 'path'; 
+import { fileURLToPath } from 'url'; 
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +26,12 @@ connectDB();
 
 app.use('/api', authRoutes);
 app.use('/api/quizzes', quizRoutes);
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+});
 
 app.get('/', (req, res) => {
 	res.send('server is running');

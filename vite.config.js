3461b9e45cb4984531182d2c554cb1944afb58a6
아수나 <105264974+asxnah@users.noreply.github.com>
2025-04-18 13:27:28 +0500

    -

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import process from 'process';
 export default defineConfig({
 	plugins: [react()],
 	define: {
-		'process.env': process.env,
+		'process.env.NODE_ENV': JSON.stringify(
+			process.env.NODE_ENV || 'development'
+		),
 	},
 });

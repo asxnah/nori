1ef2118258725099e9def4dda1a0ad59f7aa623a
아수나 <105264974+asxnah@users.noreply.github.com>
2025-04-18 12:55:10 +0500

    -

--- server/index.js
+++ server/index.js
@@ -5,16 +5,16 @@ import authRoutes from './routes/auth.js';
 import quizRoutes from './routes/quizzes.js';
 import dotenv from 'dotenv';
 import process from 'process';
-import path from 'path'; 
-import { fileURLToPath } from 'url'; 
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url); 
-const __dirname = path.dirname(__filename); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -27,15 +27,19 @@ connectDB();
 app.use('/api', authRoutes);
 app.use('/api/quizzes', quizRoutes);
 
-app.use(express.static(path.resolve(__dirname, '../dist')));
-
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
-});
-
 app.get('/', (req, res) => {
 	res.send('server is running');
 });
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.resolve(__dirname, '../dist')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+	});
+} else {
+	console.log('Running in development mode, not serving frontend.');
+}
 
 app.listen(port, '0.0.0.0', () => {
 	console.log(`Server is running on port ${port}`);

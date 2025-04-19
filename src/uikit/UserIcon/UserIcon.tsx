8f97e7c44dc8a4facc89e5f48f4670d2e415ac9d
아수나 <105264974+asxnah@users.noreply.github.com>
2025-04-19 21:12:40 +0500

    refactor: толщина stroke

--- src/uikit/UserIcon/UserIcon.tsx
+++ src/uikit/UserIcon/UserIcon.tsx
@@ -8,12 +8,12 @@ export const UserIcon = () => (
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
-		<circle cx="10" cy="10" r="9" stroke="#323232" strokeWidth="2" />
-		<circle cx="10" cy="7" r="4" stroke="#323232" strokeWidth="2" />
+		<circle cx="10" cy="10" r="9" stroke="#323232" strokeWidth="1.25" />
+		<circle cx="10" cy="7" r="4" stroke="#323232" strokeWidth="1.25" />
 		<path
 			d="M3 16L3.60638 15.5669C7.43105 12.835 12.5689 12.835 16.3936 15.5669L17 16"
 			stroke="#323232"
-			strokeWidth="2"
+			strokeWidth="1.25"
 		/>
 	</svg>
 );
